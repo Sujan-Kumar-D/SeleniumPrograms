c818a448497620e648d836da459dc8b2ae1988f2
Sujan-Kumar-D <kumar.sujand@gmail.com>
2020-07-29 15:45:38 +0530

    Bootstrap

--- AllPrograms/src/TestNGPrograms/BootStrapSpiceJet.java
+++ AllPrograms/src/TestNGPrograms/BootStrapSpiceJet.java
@@ -14,19 +14,20 @@ public class BootStrapSpiceJet {
 	WebDriver driver;
 
 	@Test
-	public void bootstrapsite() {
+	public void a_bootstrapsite() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
 				"C://Sujan//Essentials//Browser Drivers//Chrome Driver 84//chromedriver_win32//chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.spicejet.com/");
 		driver.manage().window().maximize();
+		Thread.sleep(10000);
 	}
 	
 	@Test
-	public void spicejetfrom() {
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	public void b_spicejetfrom() throws InterruptedException {
+		Thread.sleep(5000);
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-		ArrayList <WebElement> dropIndiafrom = new ArrayList <WebElement> (driver.findElements(By.xpath("(//div[@class='dropdownDiv'])[1]//ul/li/a")));
+		ArrayList <WebElement> dropIndiafrom = new ArrayList <WebElement> (driver.findElements(By.xpath("(//td[@class='mapbg'])[1]//ul/li/a")));
 		for (WebElement Indiafrom:dropIndiafrom) {
 			String IndiaDropdownValuesfrom = Indiafrom.getText();
 			System.out.println(IndiaDropdownValuesfrom);
@@ -38,9 +39,9 @@ public class BootStrapSpiceJet {
 	}
 	
 	@Test
-	public void spicejetto() {
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		ArrayList <WebElement> dropIndiato = new ArrayList <WebElement> (driver.findElements(By.xpath("(//div[@class='dropdownDiv'])[3]//ul/li/a")));
+	public void c_spicejetto() throws InterruptedException {
+		Thread.sleep(5000);
+		ArrayList <WebElement> dropIndiato = new ArrayList <WebElement> (driver.findElements(By.xpath("(//td[@class='mapbg'])[2]//ul/li/a")));
 		for (WebElement Indiato:dropIndiato) {
 			String IndiaDropdownValuesto = Indiato.getText();
 			System.out.println(IndiaDropdownValuesto);
@@ -52,6 +53,24 @@ public class BootStrapSpiceJet {
 		
 	}
 	
-	
+	@Test
+	public void d_spicejetdeparturedate() {
+		for(int i=0;i<3;i++) {
+			driver.findElement(By.xpath("//span[text()='Next']")).click();
+		}
+			//driver.findElement(By.xpath("(//table[@class='ui-datepicker-calendar'])[2]//a[text()='10']")).click();
+			ArrayList <WebElement> dates = new ArrayList <WebElement> (driver.findElements(By.xpath("(//table[@class='ui-datepicker-calendar'])[2]//td/a")));
+			for(WebElement datelist: dates) {
+				String datevalues = datelist.getText();
+				System.out.println(datevalues);
+				if(datevalues.equals("10")) {
+					datelist.click();
+					break;
+				}
+				
+			}
+			
+		}
+		
 	}
 
--- AllPrograms/src/TestNGPrograms/OrangeHRM.java
+++ AllPrograms/src/TestNGPrograms/OrangeHRM.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -22,12 +23,12 @@ public class OrangeHRM {
 		driver.get("https://orangehrm-demo-6x.orangehrmlive.com/");
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test
 	public void b_login() {
 		driver.findElement(By.id("btnLogin")).submit();
 	}
-	
+
 	@Test
 	public void c_addEmployeeStep() throws InterruptedException {
 		driver.findElement(By.xpath("//span[contains(text(),'PIM')]")).click();
@@ -38,146 +39,158 @@ public class OrangeHRM {
 		driver.findElement(By.id("employeeId")).clear();
 		driver.findElement(By.id("employeeId")).sendKeys("12345");
 		driver.findElement(By.xpath("//div[@id='location_inputfileddiv']")).click();
-		//Country Dropdown
-		ArrayList <WebElement> Country = new ArrayList <WebElement> (driver.findElements(By.xpath("//div[@id='location_inputfileddiv']//ul/li")));
-		for(WebElement Countryoptions:Country) {
+		// Country Dropdown
+		ArrayList<WebElement> Country = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("//div[@id='location_inputfileddiv']//ul/li")));
+		for (WebElement Countryoptions : Country) {
 			String dropdownvalues = Countryoptions.getText();
-			System.out.println(dropdownvalues);}
-		for(WebElement Countryoptions:Country) {
+			System.out.println(dropdownvalues);
+		}
+		for (WebElement Countryoptions : Country) {
 			String dropdownvalues = Countryoptions.getText();
-			if(dropdownvalues.equalsIgnoreCase("     German Regional HQ")) {
+			if (dropdownvalues.equalsIgnoreCase("     German Regional HQ")) {
 				Countryoptions.click();
 				break;
 			}
 		}
+		Thread.sleep(2000);
 		driver.findElement(By.id("systemUserSaveBtn")).click();
 	}
-	
+
 	@Test
 	public void d_personaldetails() throws InterruptedException {
 		Thread.sleep(15000);
 		driver.findElement(By.xpath("//label[text()='Marital Status']/preceding::i[1]")).click();
-		//Year dropdown in Date Of Birth
+		// Year dropdown in Date Of Birth
 		driver.findElement(By.xpath("(//div[@class='select-wrapper picker__select--year'])[1]")).click();
-		ArrayList <WebElement> dobyear = new ArrayList <WebElement> (driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--year'])[1]//ul/li")));
-		for(WebElement yearlist: dobyear) {
+		ArrayList<WebElement> dobyear = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--year'])[1]//ul/li")));
+		for (WebElement yearlist : dobyear) {
 			String yeardropdown = yearlist.getText();
 			System.out.println(yeardropdown);
 		}
-		for(WebElement yearlist: dobyear) {
+		for (WebElement yearlist : dobyear) {
 			String yeardropdown = yearlist.getText();
-			if(yeardropdown.equalsIgnoreCase("1988")) {
+			if (yeardropdown.equalsIgnoreCase("1988")) {
 				yearlist.click();
 				break;
 			}
 		}
-		//Month dropdown in Date Of Birth
+		// Month dropdown in Date Of Birth
 		driver.findElement(By.xpath("(//div[@class='select-wrapper picker__select--month'])[1]")).click();
-		ArrayList <WebElement> dobmonth = new ArrayList <WebElement> (driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--month'])[1]//ul/li")));
-		for(WebElement monthlist: dobmonth) {
+		ArrayList<WebElement> dobmonth = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--month'])[1]//ul/li")));
+		for (WebElement monthlist : dobmonth) {
 			String monthdropdown = monthlist.getText();
 			System.out.println(monthdropdown);
 		}
-		for(WebElement monthlist: dobmonth) {
+		for (WebElement monthlist : dobmonth) {
 			String monthdropdown = monthlist.getText();
-			if(monthdropdown.equalsIgnoreCase("June")) {
+			if (monthdropdown.equalsIgnoreCase("June")) {
 				monthlist.click();
 				break;
 			}
 		}
-		//Date Selection in Date Of Birth
+		// Date Selection in Date Of Birth
 		driver.findElement(By.xpath("(//div[text()='10'])[1]")).click();
-		
-		//Marital Status dropdown
+
+		// Marital Status dropdown
 		driver.findElement(By.id("emp_marital_status_inputfileddiv")).click();
-		ArrayList <WebElement> marrital = new ArrayList <WebElement> (driver.findElements(By.xpath("//div[@id='emp_marital_status_inputfileddiv']//ul/li")));
-		for(WebElement maritallist: marrital) {
+		ArrayList<WebElement> marrital = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("//div[@id='emp_marital_status_inputfileddiv']//ul/li")));
+		for (WebElement maritallist : marrital) {
 			String marvalues = maritallist.getText();
 			System.out.println(marvalues);
 		}
-		for(WebElement maritallist: marrital) {
+		for (WebElement maritallist : marrital) {
 			String marvalues = maritallist.getText();
-			if(marvalues.equalsIgnoreCase("Married")) {
+			if (marvalues.equalsIgnoreCase("Married")) {
 				maritallist.click();
 				break;
 			}
-	}
-		//Gender dropdown
+		}
+		// Gender dropdown
 		driver.findElement(By.id("emp_gender_inputfileddiv")).click();
-		ArrayList <WebElement> gender = new ArrayList <WebElement> (driver.findElements(By.xpath("//div[@id='emp_gender_inputfileddiv']//ul/li")));
-		for(WebElement genderlist: gender) {
+		ArrayList<WebElement> gender = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("//div[@id='emp_gender_inputfileddiv']//ul/li")));
+		for (WebElement genderlist : gender) {
 			String gendervalues = genderlist.getText();
 			System.out.println(gendervalues);
 		}
-		for(WebElement genderlist: gender) {
+		for (WebElement genderlist : gender) {
 			String gendervalues = genderlist.getText();
-			if(gendervalues.equalsIgnoreCase("Male")) {
+			if (gendervalues.equalsIgnoreCase("Male")) {
 				genderlist.click();
 				break;
 			}
-	}
-		//Nationality dropdown
+		}
+		// Nationality dropdown
 		driver.findElement(By.id("nation_code_inputfileddiv")).click();
-		ArrayList <WebElement> nation = new ArrayList <WebElement> (driver.findElements(By.xpath("//div[@id='nation_code_inputfileddiv']//ul/li")));
-		for(WebElement nationlist: nation) {
+		ArrayList<WebElement> nation = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("//div[@id='nation_code_inputfileddiv']//ul/li")));
+		for (WebElement nationlist : nation) {
 			String nationvalues = nationlist.getText();
 			System.out.println(nationvalues);
 		}
-		for(WebElement nationlist: nation) {
+		for (WebElement nationlist : nation) {
 			String nationvalues = nationlist.getText();
-			if(nationvalues.equalsIgnoreCase("Indian")) {
+			if (nationvalues.equalsIgnoreCase("Indian")) {
 				nationlist.click();
 				break;
 			}
-	}
+		}
 		driver.findElement(By.id("licenseNo")).sendKeys("123456");
-		
-		//Year dropdown in License Expiry
+
+		// Year dropdown in License Expiry
 		driver.findElement(By.xpath("//label[text()='Nick Name']/preceding::i[1]")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("(//div[@class='select-wrapper picker__select--year'])[2]")).click();
-		ArrayList <WebElement> licnseyear = new ArrayList <WebElement> (driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--year'])[2]//ul/li")));
-		for(WebElement yearlist: licnseyear) {
+		ArrayList<WebElement> licnseyear = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--year'])[2]//ul/li")));
+		for (WebElement yearlist : licnseyear) {
 			String yeardropdown = yearlist.getText();
 			System.out.println(yeardropdown);
 		}
-		for(WebElement yearlist: licnseyear) {
+		for (WebElement yearlist : licnseyear) {
 			String yeardropdown = yearlist.getText();
-			if(yeardropdown.equalsIgnoreCase("2030")) {
+			if (yeardropdown.equalsIgnoreCase("2030")) {
 				yearlist.click();
 				break;
 			}
 		}
-		//Month dropdown in License Expiry
+		// Month dropdown in License Expiry
 		driver.findElement(By.xpath("(//div[@class='select-wrapper picker__select--month'])[2]")).click();
-		ArrayList <WebElement> licnsemonth = new ArrayList <WebElement> (driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--month'])[2]//ul/li")));
-		for(WebElement monthlist: licnsemonth) {
+		ArrayList<WebElement> licnsemonth = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--month'])[2]//ul/li")));
+		for (WebElement monthlist : licnsemonth) {
 			String monthdropdown = monthlist.getText();
 			System.out.println(monthdropdown);
 		}
-		for(WebElement monthlist: licnsemonth) {
+		for (WebElement monthlist : licnsemonth) {
 			String monthdropdown = monthlist.getText();
-			if(monthdropdown.equalsIgnoreCase("June")) {
+			if (monthdropdown.equalsIgnoreCase("June")) {
 				monthlist.click();
 				break;
 			}
 		}
-		//Date Selection in License Expiry
+		// Date Selection in License Expiry
 		driver.findElement(By.xpath("(//div[text()='15'])[2]")).click();
-		
+
 		driver.findElement(By.id("nickName")).sendKeys("sachin");
 		driver.findElement(By.id("militaryService")).sendKeys("yes");
-		
-		//Bloodgroup dropdown
+		Thread.sleep(2000);
+
+		// Bloodgroup dropdown
 		driver.findElement(By.id("1_inputfileddiv")).click();
-		ArrayList <WebElement> bloodgrp = new ArrayList <WebElement> (driver.findElements(By.xpath("//div[@id='1_inputfileddiv']//ul/li")));
-		for(WebElement bloodgrplist: bloodgrp) {
+		ArrayList<WebElement> bloodgrp = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("//div[@id='1_inputfileddiv']//ul/li")));
+		for (WebElement bloodgrplist : bloodgrp) {
 			String bloodgrpvalues = bloodgrplist.getText();
 			System.out.println(bloodgrpvalues);
 		}
-		for(WebElement bloodgrplist: bloodgrp) {
+		for (WebElement bloodgrplist : bloodgrp) {
 			String bloodgrpvalues = bloodgrplist.getText();
-			if(bloodgrpvalues.equalsIgnoreCase("O")) {
+			if (bloodgrpvalues.equalsIgnoreCase("O")) {
 				bloodgrplist.click();
 				break;
 			}
@@ -185,6 +198,88 @@ public class OrangeHRM {
 		driver.findElement(By.id("5")).sendKeys("Cricket");
 		driver.findElement(By.xpath("//button[text()='Next']")).click();
 	}
+
+	@Test
+	public void e_employeedetails() throws InterruptedException {
+		Thread.sleep(20000);
+		// Joining Date Dropdown
+		driver.findElement(By.xpath("//label[text()='Date of Permanency']//preceding::i[1]")).click();
+		driver.findElement(By.xpath("(//div[@class='select-wrapper picker__select--year'])[1]")).click();
+		ArrayList<WebElement> joinyear = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--year'])[1]//ul/li")));
+		for (WebElement joinyearlist : joinyear) {
+			String joinyeardrop = joinyearlist.getText();
+			System.out.println(joinyeardrop);
+		}
+		for (WebElement joinyearlist : joinyear) {
+			String joinyeardrop = joinyearlist.getText();
+			if (joinyeardrop.equalsIgnoreCase("2009")) {
+				joinyearlist.click();
+				break;
+			}
+		}
+		driver.findElement(By.xpath("(//div[@class='select-wrapper picker__select--month'])[1]")).click();
+		ArrayList<WebElement> joinmonth = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--month'])[1]//ul/li")));
+		for (WebElement joinmonthlist : joinmonth) {
+			String joinmonthdrop = joinmonthlist.getText();
+			System.out.println(joinmonthdrop);
+		}
+		for (WebElement joinmonthlist : joinmonth) {
+			String joinmonthdrop = joinmonthlist.getText();
+			if (joinmonthdrop.equalsIgnoreCase("December")) {
+				joinmonthlist.click();
+				break;
+			}
+		}
+		driver.findElement(By.xpath("(//div[text()='28'])[2]")).click();
+
+		// Permanency Data Dropdown
+		driver.findElement(By.xpath("//label[text()='Job Title']/preceding::i[1]")).click();
+		driver.findElement(By.xpath("(//div[@class='select-wrapper picker__select--year'])[2]")).click();
+		ArrayList<WebElement> permanyear = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--year'])[2]//ul/li")));
+		for (WebElement permanyearlist : permanyear) {
+			String permanyeardrop = permanyearlist.getText();
+			System.out.println(permanyeardrop);
+		}
+		for (WebElement permanyearlist : permanyear) {
+			String permanyeardrop = permanyearlist.getText();
+			if (permanyeardrop.equalsIgnoreCase("2019")) {
+				permanyearlist.click();
+				break;
+			}
+		}
+		driver.findElement(By.xpath("(//div[@class='select-wrapper picker__select--month'])[2]")).click();
+		ArrayList<WebElement> permanmonth = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("(//div[@class='select-wrapper picker__select--month'])[2]//ul/li")));
+		for (WebElement permanmonthlist : permanmonth) {
+			String permanmonthdrop = permanmonthlist.getText();
+			System.out.println(permanmonthdrop);
+		}
+		for (WebElement permanmonthlist : permanmonth) {
+			String permanmonthdrop = permanmonthlist.getText();
+			if (permanmonthdrop.equalsIgnoreCase("December")) {
+				permanmonthlist.click();
+				break;
+			}
+		}
+		driver.findElement(By.xpath("(//div[text()='15'])[2]")).click();
+
+		// JobTitle dropdown
+
+		driver.findElement(By.id("job_title_id_inputfileddiv")).click();
+		ArrayList<WebElement> Jobtitle = new ArrayList<WebElement>(
+				driver.findElements(By.xpath("//div[@id='job_title_id_inputfileddiv']//ul/li")));
+		for (WebElement Jobtitlelist : Jobtitle) {
+			String Jobtitledropdown = Jobtitlelist.getText();
+			System.out.println(Jobtitledropdown);
+		}
+		for (WebElement Jobtitlelist : Jobtitle) {
+			String Jobtitledropdown = Jobtitlelist.getText();
+			if (Jobtitledropdown.equalsIgnoreCase("Finance Manager")) {
+				Jobtitlelist.click();
+			}
+		}
+	}
 }
-
-
